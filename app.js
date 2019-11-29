30e7d646b23d51fb8c70b1f84d992eed01d92a89
tung1998 <tung16021220@gmail.com>
2019-11-29 13:29:41 +0700

    update ui

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-const cookie = require('cookie');
-const md5 = require('md5');
-
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
@@ -27,7 +24,9 @@ MongoClient.connect(url, {
     if (err) throw err;
     const db = client.db('arduino');
 
-    app.post('/getData', (req, res, next) => {
+
+
+    app.get('/getDataChart', (req, res, next) => {
         db.collection('data').find().limit(50).sort({
             _id: -1
         }).toArray(function (err, data) {
@@ -36,24 +35,13 @@ MongoClient.connect(url, {
         })
     })
 
-    app.get('/user', function (req, res) {
-        var cookies = cookie.parse(req.headers.cookie || '');
-        if (!cookies.name) {
-            res.redirect('/home');
-        } else {
-            db.collection("user").find(cookies).toArray(function (err, result) {
-                console.log(result);
-                if (result.length) {
-                    db.collection('data').find().limit(5).sort({
-                        _id: -1
-                    }).toArray(function (err, data) {
-                        res.render('user', {
-                            data: data
-                        });
-                    })
-                } else res.redirect('/home');
-            })
-        }
+    app.get('/getCurentData', (req, res, next) => {
+        db.collection('data').find().limit(50).sort({
+            _id: -1
+        }).toArray(function (err, data) {
+            res.send(data);
+            res.end();
+        })
     })
 
     app.get('/', function (req, res) {
@@ -61,102 +49,26 @@ MongoClient.connect(url, {
         res.end();
     });
 
-    app.post('/logout', (req, res, next) => {
-        res.clearCookie("name");
-        res.send('success');
-    })
-
-
-    app.get('/register', function (req, res) {
-        res.render('registerPage');
-        res.end();
-    });
-
-
-    app.get('/login', function (req, res) {
-        var cookies = cookie.parse(req.headers.cookie || '');
-        if (!cookies.name) {
-            res.render('loginPage');
-        } else {
-            db.collection("user").find(cookies).toArray(function (err, result) {
-                if (result.length) {
-                    res.redirect('/user');
-                } else res.render('loginPage');
-            })
-        }
-    });
-
-    app.post('/register', (req, res) => {
-        var data = req.body;
-        data.name = md5(data.username + data.password);
-        db.collection("user").insertOne(data, function (err, result) {
-            if (err) console.log("loi");
-            console.log("1 document inserted");
-            res.send('1');
-            res.end();
-        });
-    });
-
-
-    app.post('/login', function (req, res) {
-        var data = req.body
-        db.collection("user").find(data).toArray(function (err, result) {
-            if (err) throw err;
-            if (result.length) {
-                res.setHeader('Set-Cookie', cookie.serialize('name', result[0].name, {
-                    httpOnly: true,
-                    maxAge: 60 * 60 * 24 * 7 // 1 week
-                }));
-                res.send('1');
-                res.end();
-            } else res.send('0');
-        })
-    });
-
     app.get('/home', function (req, res) {
-        var cookies = cookie.parse(req.headers.cookie || '');
-        db.collection('data').find().limit(1).sort({
-            _id: -1
-        }).toArray(function (err, data) {
+        res.render('home', {})
 
-            if (!cookies.name) {
-                res.render('home', {
-                    data: data
-                });
-            } else {
-                db.collection("user").find(cookies).toArray(function (err, result) {
-                    // console.log(result);
-                    if (result.length) {
-                        res.redirect('/user');
-                    } else res.render('home', {
-                        data: data
-                    });
-                })
+        app.get('/getData', (req, res) => {
+            let data = {
+                dht22_t: Number(req.query.dht22_t),
+                dht22_h: Number(req.query.dht22_h),
+                gy68_t: Number(req.query.gy68_t),
+                gy68_p: Number(req.query.gy68_p),
+                createdTime: Date.now()
             }
-        })
-
-    })
-
-
-    app.get('/getData', (req, res) => {
-        let data = {
-            dht22_t:Number(req.query.dht22_t),
-            dht22_h:Number(req.query.dht22_h),
-            gy68_t:Number(req.query.gy68_t),
-            gy68_p:Number(req.query.gy68_p),
-            createdTime: Date.now()
-        }
-        // console.log(data)
-        db.collection("data").insertOne(data, function (err, result) {
-            if (err) console.log("loi");
-            console.log("1 document inserted");
-            res.send('ok')
-            res.end();
+            // console.log(data)
+            db.collection("data").insertOne(data, function (err, result) {
+                if (err) console.log("loi");
+                console.log("1 document inserted");
+                res.send('ok')
+                res.end();
+            });
         });
-    });
-
-
-
+    })
 
     const server = app.listen(3000, (err) => {
         if (err) throw err;
@@ -165,7 +77,5 @@ MongoClient.connect(url, {
 
         console.log("Example app listening at http://%s:%s", host, port);
     });
-
-
-
 });
+
--- file/script.js
+++ file/script.js
@@ -1,203 +1,76 @@
 var chart;
 var chartData = [];
 
-$(document).ready(function() {
-    $("#login").click(function () {
-        window.location.replace("/login");
-    });
+$(document).ready(function () {
+    // setInterval(function () {
 
-
-    $("#register").click(function () {
-        window.location.replace("/register");
-    });
-
-    $("#logout").click(function () {
-        $.ajax({
-            url:'/logout',
-            type:'post',
-            success: function(){
-                location.reload();
-            }
-        });
-    });
-
-
-
-
+    //     reloadData()
+    // }, 5000)
+    renderChart()
+    resizePage()
 });
 
 
-AmCharts.ready(function () {
-    console.log(data);
-
-    generateChartData(data);
-
-    // SERIAL CHART
-    chart = new AmCharts.AmSerialChart();
-
-
-    chart.dataProvider = chartData;
-    chart.categoryField = "date";
-
-    // listen for "dataUpdated" event (fired when chart is inited) and call zoomChart method when it happens
-    chart.addListener("dataUpdated", zoomChart);
-
-    chart.synchronizeGrid = true; // this makes all axes grid to be at the same intervals
-
-    // AXES
-    // category
-    var categoryAxis = chart.categoryAxis;
-    categoryAxis.parseDates = true; // as our data is date-based, we set parseDates to true
-    categoryAxis.minPeriod = "DD"; // our data is daily, so we set minPeriod to DD
-    categoryAxis.minorGridEnabled = true;
-    categoryAxis.axisColor = "#DADADA";
-    categoryAxis.twoLineMode = true;
-    categoryAxis.dateFormats = [{
-        period: 'fff',
-        format: 'JJ:NN:SS'
-    }, {
-        period: 'ss',
-        format: 'JJ:NN:SS'
-    }, {
-        period: 'mm',
-        format: 'JJ:NN'
-    }, {
-        period: 'hh',
-        format: 'JJ:NN'
-    }, {
-        period: 'DD',
-        format: 'DD'
-    }, {
-        period: 'WW',
-        format: 'DD'
-    }, {
-        period: 'MM',
-        format: 'MMM'
-    }, {
-        period: 'YYYY',
-        format: 'YYYY'
-    }];
-
-    // first value axis (on the left)
-    var valueAxis1 = new AmCharts.ValueAxis();
-    valueAxis1.axisColor = "#FF6600";
-    valueAxis1.axisThickness = 2;
-    valueAxis1.autoGridCount = false;
-    valueAxis1.gridCount = 10;
-    chart.addValueAxis(valueAxis1);
+function reloadData() {
+    $.ajax({
+        async: false,
+        type: 'post',
+        url: '/getData',
+        contentType: "application/json",
+        success: function (data) {
+            // data=JSON.parse(data);
+            console.log(data);
+            generateChartData(data);
+            chart.dataProvider = chartData;
+            chart.validateData();
+        }
+    })
+}
 
 
-    // second value axis (on the right)
-    var valueAxis2 = new AmCharts.ValueAxis();
-    valueAxis2.position = "right"; // this line makes the axis to appear on the right
-    valueAxis2.axisColor = "#FCD202";
-    valueAxis2.gridAlpha = 0;
-    valueAxis2.axisThickness = 2;
-    valueAxis2.autoGridCount = false;
-    valueAxis2.gridCount = 10;
-    chart.addValueAxis(valueAxis2);
-
-    // GRAPHS
-    // first graph
-    var graph1 = new AmCharts.AmGraph();
-    graph1.valueAxis = valueAxis1; // we have to indicate which value axis should be used
-    graph1.title = "humi";
-    graph1.valueField = "humi";
-    graph1.bullet = "round";
-    graph1.hideBulletsCount = 30;
-    graph1.bulletBorderThickness = 1;
-    chart.addGraph(graph1);
-
-    // second graph
-    var graph2 = new AmCharts.AmGraph();
-    graph2.valueAxis = valueAxis2; // we have to indicate which value axis should be used
-    graph2.title = "temc";
-    graph2.valueField = "temc";
-    graph2.bullet = "square";
-    graph2.hideBulletsCount = 30;
-    graph2.bulletBorderThickness = 1;
-    chart.addGraph(graph2);
-
-    // CURSOR
-    var chartCursor = new AmCharts.ChartCursor();
-    chartCursor.cursorAlpha = 0.1;
-    chartCursor.fullWidth = true;
-    chartCursor.valueLineBalloonEnabled = true;
-    chart.addChartCursor(chartCursor);
-
-    // SCROLLBAR
-    var chartScrollbar = new AmCharts.ChartScrollbar();
-    chart.addChartScrollbar(chartScrollbar);
-
-    // LEGEND
-    var legend = new AmCharts.AmLegend();
-    legend.marginLeft = 110;
-    legend.useGraphSettings = true;
-    chart.addLegend(legend);
-
-
-
-
-    // WRITE
-    chart.write("chartdiv");
-    // $(document).ready(function() {
-    setInterval(function () {
-        $.ajax({
-            async: false,
-            type : 'post',
-            url : '/getData',
-            contentType: "application/json",
-            success: function(data){
-                // data=JSON.parse(data);
-                console.log(data);
-                generateChartData(data);
-                chart.dataProvider = chartData;
-                chart.validateData();
+function renderChart() {
+    var ctx = document.getElementById('chartData').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
             }
-        })
-    },5000)
-    // });
-});
-
-// generate some random data, quite different range
-function generateChartData(data) {
-    var firstDate = new Date();
-    firstDate.setDate(firstDate.getDate() - 5);
-    chartData=[];
-    for (var i = 4; i >=0; i--) {
-        // we create date objects here. In your data, you can have date strings
-        // and then set format of your dates using chart.dataDateFormat property,
-        // however when possible, use date objects, as this will speed up chart rendering.
-        var newDate = new Date(firstDate);
-        newDate.setDate(newDate.getDate() - i);
-        chartData.push({
-            date: newDate,
-            humi: data[i].humi,
-            temc: data[i].temc,
-            temf: data[i].temf
-        });
-    }
+        }
+    });
 }
 
-
-
-// this method is called when chart is first inited as we listen for "dataUpdated" event
-function zoomChart() {
-    // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
-    chart.zoomToIndexes(0, 100);
+function resizePage(){
+    let screenHeight = window.innerHeight
+    let headerheight = $('#lastData').outerHeight()
+    $('#chartData').css({'max-height':`${(screenHeight-headerheight-30)}px`})
+    console.log(screenHeight,headerheight)
 }
-//
-//
-// $(document).ready(function() {
-//     setInterval(function () {
-//         $.ajax({
-//             type : 'get',
-//             url : '/data',
-//             contentType: "application/json",
-//             success: function(data){
-//                 JSON.parse(data);
-//                 generateChartData(data);
-//             }
-//         })
-//     },5000)
-// });
